347acc84571fa7f2b9f9a5221e1bd969607eacb0
Shankar Rao Pandala <shankar.pandala@gmail.com>
2021-02-23 20:31:52 +0000

    updated train

--- train.py
+++ train.py
@@ -1,4 +1,4 @@
-from sklearn.linear_model import LogisticRegression
+from sklearn.ensemble import RandomForestRegressor
 import argparse
 import os
 import numpy as np
@@ -9,49 +9,34 @@ from sklearn.preprocessing import OneHotEncoder
 import pandas as pd
 from azureml.core.run import Run
 from azureml.data.dataset_factory import TabularDatasetFactory
-
-def clean_data(data):
-    # Clean and one hot encode data
-    x_df = data.to_pandas_dataframe().dropna()
-    print(x_df.shape)
-    
-    # TODO: scale data
-
-    y_df = x_df.pop("DEATH_EVENT")
-
-    print(x_df.shape)
-    print(y_df.shape)
-
-    return x_df, y_df
-
-# TODO: Create TabularDataset using TabularDatasetFactory
-# Data is located at:
-# "https://automlsamplenotebookdata.blob.core.windows.net/automl-sample-notebook-data/bankmarketing_train.csv"
-
-path = "https://raw.githubusercontent.com/Arushikha0408/nd00333-capstone/master/heart_failure_clinical_records_dataset.csv"
-ds = TabularDatasetFactory.from_delimited_files(path, validate=True, include_path=False, infer_column_types=True, set_column_types=None, separator=',', header=True, partition_format=None, support_multi_line=False, empty_as_string=False)
-
-### YOUR CODE HERE ###
-x, y = clean_data(ds)
-# TODO: Split data into train and test sets.
-x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=36)
+from azureml.core import Dataset, Run
+dataset_name = 'bmw_cars'
 run = Run.get_context()
+ws = run.experiment.workspace
+# Get a dataset by name
+bmw_cars = Dataset.get_by_name(workspace=ws, name=dataset_name)
+
+# Load a TabularDataset into pandas DataFrame
+df = bmw_cars.to_pandas_dataframe()
+y = df.price
+X = df.drop(['price'], axis=1)
+x_train, x_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=36)
+
 
 def main():
     # Add arguments to script
     parser = argparse.ArgumentParser()
 
-    parser.add_argument('--C', type=float, default=1.0, help="Inverse of regularization strength. Smaller values cause stronger regularization")
-    parser.add_argument('--max_iter', type=int, default=100, help="Maximum number of iterations to converge")
+    parser.add_argument('--n_estimators', type=int)
+    parser.add_argument('--max_features', type=int)
+    parser.add_argument('--max_depth', type=int)
+    parser.add_argument('--min_samples_split', type=int)
 
     args = parser.parse_args()
 
-    run.log("Regularization Strength:", np.float(args.C))
-    run.log("Max iterations:", np.int(args.max_iter))
-
-    model = LogisticRegression(C=args.C, max_iter=args.max_iter).fit(x_train, y_train)
-    accuracy = model.score(x_test, y_test)
-    run.log("Accuracy", np.float(accuracy))
+    model = RandomForestRegressor(n_estimators=args.n_estimators, max_features=args.max_features, max_depth=args.max_depth, min_samples_split=args.min_samples_split).fit(x_train, y_train)
+    metric = model.score(x_test, y_test)
+    run.log("metric", np.float(metric))
     os.makedirs('./outputs', exist_ok=True)
     joblib.dump(value=model, filename='./outputs/hd-model.joblib')
 
