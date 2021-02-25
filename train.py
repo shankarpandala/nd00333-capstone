19c45d88643f757ddd7d4cfe50b555b28354e734
Shankar Rao Pandala <shankar.pandala@gmail.com>
2021-02-25 07:08:15 +0000

    AutoML deployed

--- train.py
+++ train.py
@@ -5,7 +5,9 @@ import numpy as np
 from sklearn.metrics import mean_squared_error
 import joblib
 from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import OneHotEncoder
+from sklearn.pipeline import Pipeline
+from sklearn.impute import SimpleImputer
+from sklearn.preprocessing import StandardScaler, OneHotEncoder
 import pandas as pd
 from azureml.core.run import Run
 from azureml.data.dataset_factory import TabularDatasetFactory
@@ -33,8 +35,31 @@ def main():
     parser.add_argument('--min_samples_split', type=int)
 
     args = parser.parse_args()
+    # b. get the names of the numeric anc categorical columns
+    numeric_features = x_train.select_dtypes(include=['int64', 'float64']).columns
+    categorical_features = x_train.select_dtypes(include=['object', 'category', 'period[M]']).columns
+    # create a transformer for the categorical values
+    categorical_transformer = Pipeline(steps=[
+        ('imputer', SimpleImputer(strategy='constant', fill_value='missing')),
+        ('one_hot', OneHotEncoder())])
 
-    model = RandomForestRegressor(n_estimators=args.n_estimators, max_features=args.max_features, max_depth=args.max_depth, min_samples_split=args.min_samples_split).fit(x_train, y_train)
+    # create a transformed for the numerical values
+    numeric_transformer = Pipeline(steps=[
+        ('imputer', SimpleImputer(strategy='median')),
+        ('scaler', StandardScaler())])
+
+    from sklearn.compose import ColumnTransformer
+
+    preprocessor = ColumnTransformer(
+        transformers=[
+            ('num', numeric_transformer, numeric_features),
+            ('cat', categorical_transformer, categorical_features)
+        ])
+
+    clf = Pipeline(steps=[('preprocessor', preprocessor),
+                        ('regressor', RandomForestRegressor(n_estimators=args.n_estimators, max_features=args.max_features, max_depth=args.max_depth, min_samples_split=args.min_samples_split))])
+
+    model = clf.fit(x_train, y_train)
     metric = model.score(x_test, y_test)
     run.log("metric", np.float(metric))
     os.makedirs('./outputs', exist_ok=True)
